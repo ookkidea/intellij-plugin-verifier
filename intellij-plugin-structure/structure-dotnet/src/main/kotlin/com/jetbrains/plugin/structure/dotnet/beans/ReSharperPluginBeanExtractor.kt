ebb703e8f0ca593078b2e0b9e22fbf6f929355e6
satamas <semyon.atamas@jetbrains.com>
2018-07-23 16:11:40 +0300

    .NET: protect from XXE injection

--- intellij-plugin-structure/structure-dotnet/src/main/kotlin/com/jetbrains/plugin/structure/dotnet/beans/ReSharperPluginBeanExtractor.kt
+++ intellij-plugin-structure/structure-dotnet/src/main/kotlin/com/jetbrains/plugin/structure/dotnet/beans/ReSharperPluginBeanExtractor.kt
@@ -4,10 +4,16 @@ import org.jdom2.Element
 import org.jdom2.Namespace
 import org.jdom2.input.SAXBuilder
 import org.jonnyzzz.kotlin.xml.bind.jdom.JDOM
+import org.xml.sax.InputSource
+import java.io.CharArrayReader
 import java.io.InputStream
 
+private val EMPTY_CHAR_ARRAY = CharArray(0)
+
 fun extractPluginBean(inputStream: InputStream): ReSharperPluginBean {
-  val rootElement = SAXBuilder().build(inputStream).rootElement
+  val builder = SAXBuilder()
+  builder.setEntityResolver({ _, _ -> InputSource(CharArrayReader(EMPTY_CHAR_ARRAY)) })
+  val rootElement = builder.build(inputStream).rootElement
   rootElement.namespace = Namespace.NO_NAMESPACE
   rootElement.descendants.forEach {
     if(it is Element) {
